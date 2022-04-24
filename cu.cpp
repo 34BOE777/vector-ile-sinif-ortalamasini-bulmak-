9637fd7659f6689d97a9ec8892608ed97b5df04b
34BOE777 <98213674+34BOE777@users.noreply.github.com>
2022-04-24 14:28:20 +0300

    Update cu.cpp

--- cu.cpp
+++ cu.cpp
@@ -7,8 +7,8 @@ int main()
 	int cu;
 	vector<vector<int>>matris(3,vector<int>(2));
 	vector <float> ort;
-	for(int i=0;i<3;i++){//kişi sayısı
-		for(int j=0;j<2;j++){//ders sayısı
+	for(int i=0;i<3;i++){
+		for(int j=0;j<2;j++){
 		cout<<"Öğrenci "<<j+1<<" DERS["<<i+1<<"] Notu ="<<endl;
 		cin>>cu;
 		matris[i][j]=cu;
@@ -21,7 +21,6 @@ int main()
 		toplam=toplam+matris[i][j];
 		}
 		ort.push_back(toplam/2);
-		//ort[i]=toplam/3;
 	}
 	int x=1;
 	for(int a:ort){
